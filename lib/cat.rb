4917528d73b040299a47ab8c6e87b3698fb914c6
github username <dreedsanders@gmail.com>
2021-02-25 17:42:56 -0600

    commit tree 3f64e27d15dcead804f23251f76c597a3f74fe66

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,36 @@
 class Cat
   # code goes here
+  attr_reader :name
+
+  @@cats =[]
+  #attr_accessor :owner, mood
+ 
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@cats << self
+  end
+
+  def owner=(owner)
+    @owner = owner 
+  end
+  
+  def owner
+    @owner
+  end
+
+  def mood=(mood)
+    @mood = mood
+  end
+
+
+  def mood
+    @mood
+  end
+
+  def self.all
+    @@cats
+  end
+
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,37 @@
 class Dog
+    # code goes here
+    attr_reader :name
+  
+    @@dogs =[]
+    #attr_accessor :owner, mood
+   
+    def initialize(name, owner)
+      @name = name
+      @owner = owner
+      @mood = "nervous"
+      @@dogs << self
+    end
+  
+    def owner=(owner)
+      @owner = owner 
+    end
+    
+    def owner
+      @owner
+    end
+  
+    def mood=(mood)
+      @mood = mood
+    end
+  
+  
+    def mood
+      @mood
+    end
+  
+    def self.all
+      @@dogs
+    end
+  
+  end
   # code goes here
-end
