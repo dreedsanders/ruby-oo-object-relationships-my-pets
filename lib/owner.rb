03e4c47df0c34a8eb650e7097919fcc7effdb0a6
github username <dreedsanders@gmail.com>
2021-03-02 15:28:12 -0600

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -74,7 +74,7 @@ def self.reset_all
   end
 
   def list_pets
-    ("I have #{self.Dog.count} dog(s), and #{self.Cat.count} cat(s).")
+    ("I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s).")
   end
 
 
